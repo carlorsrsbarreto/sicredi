16eafccf4122aac3f37764320d63b73b15670e0f
barreto <barreto@Skynet>
2020-01-13 19:07:10 -0200

    Ajuste na exibição da documentação dos serviços

--- sicredi-backend/src/main/java/org/sicredi/SicrediStart.java
+++ sicredi-backend/src/main/java/org/sicredi/SicrediStart.java
@@ -3,6 +3,8 @@ package org.sicredi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class SicrediStart {
+public class SicrediStart extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(SicrediStart.class, args);
@@ -26,4 +28,15 @@ public class SicrediStart {
           .paths(PathSelectors.any())                          
           .build();                                           
     }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    
+    
 }
